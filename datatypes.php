9d484030c17b6cf176bbb913234cca1ee7d2c914
chancellordelk <chancellor.delk@students.jmcss.org>
2025-03-28 09:11:18 -0500

    data

---
+++ datatypes.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    
+ <?php
+
+    //String
+    $name = 'Coding is not always fun';
+
+    //Interger
+    $name = 13;
+
+    //Float
+    $name = 4.6592;
+
+    //Boolean
+    true = 3
+    false = 4
+
+    //Array
+    $names = array("Chance","Jossemar","Josiah");
+    echo $names['2'];
+       
+ ?>
+  </body>
+</html>
